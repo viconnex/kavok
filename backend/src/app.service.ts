e6f1ff34260957f8c0372ddc0c94ebc9b65aaff1
Victor Lebrun <victor.lebrun011@gmail.com>
2020-10-13 09:26:55 +0200

    change app return

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@nestjs/common'
 export class AppService {
     getHello(): string {
         console.log('salut le monde')
-        return 'stipule'
+        return 'stipule mandibule'
     }
 }
